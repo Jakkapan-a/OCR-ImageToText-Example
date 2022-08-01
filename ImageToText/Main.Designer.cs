1f7106fe3ba56f38328cc993ebb7bb649fc3d547
Jakkapan1997 <67558529+Jakkapan1997@users.noreply.github.com>
2022-08-01 23:09:41 +0700

    Add project files.

---
+++ ImageToText/Main.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ImageToText
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemLoadImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.rtbOcrResult = new System.Windows.Forms.RichTextBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(760, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuItem
+            // 
+            this.MenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemLoadImage,
+            this.MenuItemExit});
+            this.MenuItem.Name = "MenuItem";
+            this.MenuItem.Size = new System.Drawing.Size(37, 20);
+            this.MenuItem.Text = "File";
+            // 
+            // MenuItemLoadImage
+            // 
+            this.MenuItemLoadImage.Name = "MenuItemLoadImage";
+            this.MenuItemLoadImage.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemLoadImage.Text = "Load Image";
+            this.MenuItemLoadImage.Click += new System.EventHandler(this.MenuItemLoadImage_Click);
+            // 
+            // MenuItemExit
+            // 
+            this.MenuItemExit.Name = "MenuItemExit";
+            this.MenuItemExit.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemExit.Text = "Exit";
+            this.MenuItemExit.Click += new System.EventHandler(this.MenuItemExit_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 487);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(760, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.StatusLabel.Text = "Ready";
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(12, 27);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(385, 426);
+            this.PictureBox.TabIndex = 2;
+            this.PictureBox.TabStop = false;
+            // 
+            // rtbOcrResult
+            // 
+            this.rtbOcrResult.Location = new System.Drawing.Point(404, 28);
+            this.rtbOcrResult.Name = "rtbOcrResult";
+            this.rtbOcrResult.Size = new System.Drawing.Size(344, 425);
+            this.rtbOcrResult.TabIndex = 3;
+            this.rtbOcrResult.Text = "";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(627, 460);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(121, 23);
+            this.BtnStart.TabIndex = 4;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 509);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.rtbOcrResult);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(776, 548);
+            this.MinimumSize = new System.Drawing.Size(776, 548);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image To Text";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemLoadImage;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemExit;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.RichTextBox rtbOcrResult;
+        private System.Windows.Forms.Button BtnStart;
+    }
+}
+
---
+++ ImageToText/Main.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Tesseract;
+
+namespace ImageToText
+{
+    public partial class Main : Form
+    {
+        string part;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemLoadImage_Click(object sender, EventArgs e)
+        {
+            rtbOcrResult.Clear();
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file";
+            ofd.Filter = "Image Files(*.jpg;*.jpeg;*.png;*.bmp;*.gif)|*.png; *.jpg; *.bmp; *.gif";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                part = System.IO.Path.GetFullPath(ofd.FileName);
+                PictureBox.Image = Image.FromFile(part);
+                PictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                StatusLabel.Text = part+ " loaded";
+            }
+        }
+
+        private void MenuItemExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (PictureBox.Image == null)
+                {
+                    MessageBox.Show("Please load an image first");
+                    return;
+                }
+                else
+                {
+                    StatusLabel.Text = "OCR in progress...";
+                    rtbOcrResult.Clear();
+                    using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                    {
+                        using (var img = Pix.LoadFromFile(part))
+                        {
+                            using (var page = engine.Process(img))
+                            {
+                                var text = page.GetText();
+                                rtbOcrResult.Text = text;
+                            }
+                        }
+                    }
+                    StatusLabel.Text = "OCR completed";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                StatusLabel.Text = "OCR failed";
+            }
+        }
+    }
+}
